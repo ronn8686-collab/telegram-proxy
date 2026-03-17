294d55d18fdbc4a9f235dee97c12cf67cf159e79
ronn8686-collab <ronn8686@gmail.com>
2026-03-17 23:55:30 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,24 +1,24 @@
-export default async function handler(req, res) {
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler(req) {
   try {
     const GAS_URL = "https://script.google.com/macros/s/AKfycbzEzQzg4FXVkTV98o73uUtp90uEmKJM7R1sJMksS1cBp-Ul64qk2S1GcQ5JTcoJ68Qq8g/exec";
 
-    const rawBody = await new Promise((resolve) => {
-      let data = "";
-      req.on("data", chunk => data += chunk);
-      req.on("end", () => resolve(data));
-    });
+    const body = await req.text();
 
-    fetch(GAS_URL, {
+    await fetch(GAS_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: rawBody
+      body: body
     });
 
-    res.status(200).send("OK");
+    return new Response("OK");
 
-  } catch (err) {
-    res.status(200).send("ERROR");
+  } catch (e) {
+    return new Response("ERROR");
   }
 }
